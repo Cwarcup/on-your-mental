ddf8fcfa3f9c636ef2db8d60a6da42280f6808f5
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-02 16:41:41 -0700

    embedded videos

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -4,10 +4,12 @@ import { getMDXComponent } from 'mdx-bundler/client'
 import Image from './Image'
 import CustomLink from './Link'
 import TOCInline from './TOCInline'
+import YoutubeEmbed from './YoutubeEmbed'
 import Pre from './Pre'
 import { BlogNewsletterForm } from './NewsletterForm'
 
 export const MDXComponents = {
+  YoutubeEmbed,
   Image,
   TOCInline,
   a: CustomLink,
