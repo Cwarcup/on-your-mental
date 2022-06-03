9595a13016692c0dc5cf276d2c37a24994b49fd8
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-03 09:23:50 -0700

    height full screen

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,5 @@
 export default function SectionContainer({ children }) {
-  return <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return (
+    <div className="mx-auto h-screen max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  )
 }
