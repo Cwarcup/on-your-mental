cc75a329a368f6032392f55302c672ac1f3ce25d
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-08 09:13:14 -0700

    can get response object

--- components/videoDetail.js
+++ components/videoDetail.js
@@ -1,67 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-// const VideoDetail = () => {
-//   const requestOptions = {
-//     method: 'GET',
-//     redirect: 'follow',
-//   }
-
-//   const videos = fetch(
-//     'https://www.googleapis.com/youtube/v3/search?channelId=UCYZNw4kprBNpxBrTcnNl_Kw&part=snippet&order=date&part=snippet&key=AIzaSyAZrepPyCkcm2BXKqPK-4shzVUEA7Fd1uM\n',
-//     requestOptions
-//   )
-//     .then((response) => response.text())
-//     .then((result) => console.log(result))
-//     .catch((error) => console.log('error', error))
-
-//   return (
-//     <div>
-//       <h1>Video Detail</h1>
-//       <p></p>
-//     </div>
-//   )
-// }
-
-// export default VideoDetail
-
-// export default class VideoDetails extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       videos: [],
-//     }
-//   }
-
-//   componentDidMount() {
-//     const requestOptions = {
-//       method: 'GET',
-//       redirect: 'follow',
-//     }
-
-//     fetch(
-//       'https://www.googleapis.com/youtube/v3/search?channelId=UCYZNw4kprBNpxBrTcnNl_Kw&part=snippet&order=date&part=snippet&key=AIzaSyDB98XgVDS8xWuVQ0hojMSaII0Cxj1mw8E\n',
-//       requestOptions
-//     )
-//       .then((res) => res.json())
-//       .then((result) => {
-//         this.setState({ videos: result })
-//       })
-//       .catch((error) => console.log('error', error))
-//   }
-
-//   render() {
-//     const { videos } = this.state
-//     console.log(videos)
-
-//     return (
-//       <div>
-//         <h1>Video Detail</h1>
-//       </div>
-//     )
-//   }
-// }
-
 function VideoDetail() {
   const [error, setError] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
@@ -69,21 +8,21 @@ function VideoDetail() {
 
   var config = {
     method: 'get',
-    url: 'https://www.googleapis.com/youtube/v3/search?channelId=UCYZNw4kprBNpxBrTcnNl_Kw&part=snippet&order=date&maxResults=2&key=AIzaSyA-V6h8zGB1thGF22zOVzPLrj-EsEn4X4I',
+    url: `https://www.googleapis.com/youtube/v3/search?channelId=UCYZNw4kprBNpxBrTcnNl_Kw&part=snippet&order=date&maxResults=2&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
     headers: {},
   }
 
-  useEffect(() => {
-    axios(config)
-      .then((response) => response.data)
-      .then((data) => setItems(data))
-      .catch(function (error) {
-        console.log(error)
-      })
-  }, [])
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data))
+    })
+    // .then((response) => response.json())
+    // .then((responseData) => setItems(responseData.items))
+    .catch(function (error) {
+      console.log(error)
+    })
 
-  console.log(items.items[0].snippet.title)
-  console.log(typeof items)
+  console.log(items)
 
   return (
     <div>
