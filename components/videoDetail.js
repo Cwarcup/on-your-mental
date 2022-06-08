addac1fcc8b052a99ae3ce837fc4c17196d54d82
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-08 11:28:17 -0700

    works!!

--- components/videoDetail.js
+++ components/videoDetail.js
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function VideoDetail() {
+const VideoDetail = () => {
   const [error, setError] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [items, setItems] = useState(null)
+  const [details, setDetails] = useState()
 
-  var config = {
-    method: 'get',
-    url: `https://www.googleapis.com/youtube/v3/search?channelId=UCYZNw4kprBNpxBrTcnNl_Kw&part=snippet&order=date&maxResults=2&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
-    headers: {},
+  console.log(process.env.NEXT_PUBLIC_YOUTUBE_API_KEY)
+
+  const videoId = 'h5yUZ0MNXx4'
+
+  useEffect(() => {
+    var config = {
+      method: 'get',
+      url: `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}&fields=items(id,snippet(channelId,title,categoryId,description),statistics)&part=snippet,statistics`,
+      headers: {},
+    }
+
+    const fetchData = async () => {
+      try {
+        const result = await axios(config)
+        setDetails(result.data)
+      } catch (error) {
+        console.log('error', error)
+        setError(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  if (!details) {
+    return <h1>Loading...</h1>
   }
-
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data))
-    })
-    // .then((response) => response.json())
-    // .then((responseData) => setItems(responseData.items))
-    .catch(function (error) {
-      console.log(error)
-    })
-
-  console.log(items)
+  console.log(details)
+  // console.log(details.items[0].snippet.description) // error here. TypeError: Cannot read properties of null (reading 'items')
 
   return (
     <div>
       <h1>Video Detail</h1>
+      <p>{details.items[0].snippet.description}</p>
     </div>
   )
 }
