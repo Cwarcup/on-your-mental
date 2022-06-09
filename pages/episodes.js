eaa655777d9b81d4cbe18d043dba3314029cf08a
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-09 14:53:24 -0700

    flex col on episode container

--- pages/episodes.js
+++ pages/episodes.js
@@ -56,7 +56,7 @@ export default function Episodes() {
   return (
     <>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="flex flex-col divide-y divide-gray-200 dark:divide-gray-700 sm:content-center">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest Episode
@@ -78,11 +78,11 @@ export default function Episodes() {
 
           <AudioSocials />
         </div>
-        <div className="container py-12">
+        <div className="container py-12 ">
           <h3 className="flex pb-6 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
             Previous Episodes
           </h3>
-          <div className="-m-4 flex flex-wrap">
+          <div className=" flex flex-wrap sm:justify-center">
             {details.map((d) => (
               <Card
                 key={d.snippet.title}
