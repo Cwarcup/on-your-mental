3922054870cd4b51ffeed44c15fc98d949c5995e
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-09 18:15:41 -0700

    update social icons links

--- components/AudioSocials.js
+++ components/AudioSocials.js
@@ -9,10 +9,13 @@ export default function AudioSocials(applePodcastLink, spotifyLink, overcastLink
           <SocialIcon kind="spotify" href="https://open.spotify.com/show/5aJjICGnbefw0hxx8qW0hm" />
           <SocialIcon
             kind="applePodcasts"
-            href="https://podcasts.apple.com/ca/podcast/on-your-mental/"
+            href="https://podcasts.apple.com/ca/podcast/on-your-mental/id1551290550"
           />
-          <SocialIcon kind="castro" href="https://castro.fm/itunes15/on-your-mental" />
-          <SocialIcon kind="overcast" href="https://overcast.fm/itunes15/on-your-mental" />
+          <SocialIcon
+            kind="castro"
+            href="https://castro.fm/podcast/cc3877aa-6880-49a8-9647-f72c8eb477ce"
+          />
+          <SocialIcon kind="overcast" href="https://overcast.fm/itunes1551290550" />
         </div>
       </div>
     </>
