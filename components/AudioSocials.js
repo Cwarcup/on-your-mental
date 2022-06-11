064a0b63962d91d1253ef265351c57e100fe7fb2
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-10 23:35:06 -0700

    after reuben

--- components/AudioSocials.js
+++ components/AudioSocials.js
@@ -8,14 +8,13 @@ export default function AudioSocials(applePodcastLink, spotifyLink, overcastLink
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="spotify" href="https://open.spotify.com/show/5aJjICGnbefw0hxx8qW0hm" />
           <SocialIcon
+            kind="youtube"
+            href="https://castro.fm/podcast/cc3877aa-6880-49a8-9647-f72c8eb477ce"
+          />
+          <SocialIcon
             kind="applePodcasts"
             href="https://podcasts.apple.com/ca/podcast/on-your-mental/id1551290550"
           />
-          <SocialIcon
-            kind="castro"
-            href="https://castro.fm/podcast/cc3877aa-6880-49a8-9647-f72c8eb477ce"
-          />
-          <SocialIcon kind="overcast" href="https://overcast.fm/itunes1551290550" />
         </div>
       </div>
     </>
