7cb80a1428651431d0224b758b8d4096c606f973
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-13 16:33:09 -0700

    back to normal

--- components/Accordion.js
+++ components/Accordion.js
@@ -21,32 +21,6 @@ const Accordion = ({ title, content, description, pubDate, guidNumber }) => {
 
   const formattedDec = formatDesc(description)
 
-  const formatGuidNumber = guidNumber.replace(/\D+/g, '')
-  useEffect(() => {
-    async function getTimestamps(formatGuidNumber) {
-      axios
-        .get(`https://feeds.buzzsprout.com/1649641/${formatGuidNumber}/chapters.json`)
-        .then((res) => {
-          if (res.data.chapters !== undefined) {
-            setChapters(res.data.chapters)
-            for (let i = 0; i < chapters.length - 1; i++) {
-              // timestamp.push(secondsToTime(chapters[i].startTime))
-              console.log(chapters[i].title)
-              // chaptersArr.push(chapters[i].title)
-            }
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    }
-
-    getTimestamps(formatGuidNumber)
-  }, [formatGuidNumber])
-  console.log(chaptersArr)
-  // console.log(timestamp)
-  // console.log(chapterTitle)
-
   return (
     <div className=" accordion-item  py-6 ">
       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
@@ -58,18 +32,6 @@ const Accordion = ({ title, content, description, pubDate, guidNumber }) => {
         <div className="accordion-content transform pt-4 duration-300 ease-in-out">
           {content}
           <p className="ml-8 mr-8 pt-4 text-gray-200">{formattedDec}</p>
-          {chapterTitle ? (
-            <ul>
-              <li className="text-sm font-medium leading-5 text-gray-500 dark:text-gray-400">
-                {chapterTitle}
-              </li>
-              <li className="text-sm font-medium leading-5 text-gray-500 dark:text-gray-400">
-                {timestamp}
-              </li>
-            </ul>
-          ) : (
-            <></>
-          )}
         </div>
       </details>
     </div>
