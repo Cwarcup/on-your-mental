f41bbce00590f9410f8b75db020f7bfbf50dc184
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-14 11:46:21 -0700

    can get full description

--- components/YouTubeDescription.js
+++ components/YouTubeDescription.js
@@ -1,22 +1,33 @@
 import react, { useEffect, useState } from 'react'
-import YTVideoIdDetails from '@/lib/YTVideoIdDetails'
+import axios from 'axios'
 
 const YoutubeDescription = ({ videoId }) => {
   const [data, setData] = useState()
+  const [fullDescription, setFullDescription] = useState()
+
+  // gets data from youtube api
+  const getDescription = async (videoId) => {
+    const config = {
+      method: 'get',
+      url: `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}&fields=items(id,snippet(channelId,title,categoryId,description,thumbnails),statistics)&part=snippet,statistics`,
+      headers: {},
+    }
+
+    const response = await axios(config)
+    const data = await response.data
+    setData(data)
+    setFullDescription(data.items[0].snippet.description)
+  }
 
   useEffect(() => {
-    // You need to restrict it at some point
-    // This is just dummy code and should be replaced by actual
-    if (!data) {
-      YTVideoIdDetails().then((result) => {
-        setData(result)
-      })
-    }
+    getDescription(videoId)
   }, [])
 
   if (!data) {
     return <h1>Loading...</h1>
   }
+
+  console.log(fullDescription)
   console.log(data)
 
   return <div>full return</div>
