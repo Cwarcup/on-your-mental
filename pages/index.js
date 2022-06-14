78fe1520bc6b24fa72387cb510370d2b16e1159a
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-14 12:22:24 -0700

    border added to previous posts for home page

--- pages/index.js
+++ pages/index.js
@@ -33,9 +33,9 @@ export default function Home({ posts }) {
             return (
               <div
                 key={slug}
-                className="group bg-day dark:bg-night w-full bg-opacity-50 dark:bg-opacity-50"
+                className="group bg-day dark:bg-night w-full bg-opacity-50 dark:bg-opacity-50 "
               >
-                <div className="c-card block transform overflow-hidden rounded-lg bg-transparent transition duration-500 group-hover:scale-105">
+                <div className="c-card block transform overflow-hidden rounded-lg border-2 border-sky-900/50 bg-transparent transition duration-500 group-hover:scale-105">
                   <div className="relative max-h-4 overflow-hidden rounded-lg pb-60">
                     <span>
                       <img
@@ -45,7 +45,7 @@ export default function Home({ posts }) {
                       />
                     </span>
                   </div>
-                  <div className="py-4">
+                  <div className="py-4 px-2">
                     <span className="inline-flex w-full items-center justify-between">
                       <span className="inline-block rounded border border-gray-700 py-1 px-2 text-xs font-medium">
                         {tags.map((tag) => (
