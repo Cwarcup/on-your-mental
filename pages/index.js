dc8c0ccd85077b2d7d003eb988dc20a105cdac91
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-14 12:13:21 -0700

    description added

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Hero from '@/components/Hero'
 
 import Footer from '@/components/Footer'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 6
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
