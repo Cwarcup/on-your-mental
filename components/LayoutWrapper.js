bc6d314089cdaf4862adc94b3ddacdaf34a53c42
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-15 10:07:10 -0700

    stickt nav

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -5,15 +5,37 @@ import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
+import { useState, useEffect, useRef } from 'react'
 
 const LayoutWrapper = ({ children }) => {
+  const [stuck, setStuck] = useState(false)
+  const ref = useRef()
+
+  const stuckClasses =
+    'py-2 sticky top-n-1 z-50 transition-all backdrop isSticky mx-auto border-b border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
+  const unstuckClasses =
+    'py-2 md:py-8 sticky top-n-1 z-50 transition-all backdrop mx-auto border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
+
+  const classes = stuck ? stuckClasses : unstuckClasses
+
+  useEffect(() => {
+    const cachedRef = ref.current
+    const observer = new IntersectionObserver(
+      ([e]) => {
+        setStuck(e.intersectionRatio < 1)
+      },
+      { threshold: [1.0] }
+    )
+    observer.observe(cachedRef)
+    return () => observer.unobserve(cachedRef)
+  }, [ref])
+
   return (
-    <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-5">
+    <>
+      <header className={classes} ref={ref}>
+        <div className="mx-auto flex max-w-3xl items-center justify-between px-4 sm:px-6 xl:max-w-5xl xl:px-0">
           <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
+            <Link href="/" aria-label="On Your Mental">
               <div className="flex items-center justify-between">
                 <div className="mr-3">
                   <Logo />
@@ -22,7 +44,7 @@ const LayoutWrapper = ({ children }) => {
             </Link>
           </div>
           <div className="flex items-center text-base leading-5">
-            <div className=" hidden sm:block">
+            <div className="hidden sm:block">
               {headerNavLinks.map((link) => (
                 <Link
                   key={link.title}
@@ -35,11 +57,13 @@ const LayoutWrapper = ({ children }) => {
             </div>
             <MobileNav />
           </div>
-        </header>
+        </div>
+      </header>
+      <SectionContainer>
         <main className="mb-auto">{children}</main>
         <Footer />
-      </div>
-    </SectionContainer>
+      </SectionContainer>
+    </>
   )
 }
 
