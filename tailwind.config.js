568e1a8b779be2882a52151048af708dacfda36b
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-24 08:43:15 -0700

    daisyUI added

--- tailwind.config.js
+++ tailwind.config.js
@@ -178,5 +178,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('daisyui')],
 }
