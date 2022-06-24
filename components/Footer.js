fc35e95f24ced43844df7fcf1b38710c8f80be6e
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-24 09:21:10 -0700

    footer changed

--- components/Footer.js
+++ components/Footer.js
@@ -4,47 +4,38 @@ import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="mt-16 flex flex-col items-center">
-        <div className="mb-5 flex space-x-4">
-          <SocialIcon
-            kind="mail"
-            href={`mailto:${siteMetadata.email}`}
-            hoverColor="dark:hover:text-primary-700"
-          />
-          <SocialIcon
-            kind="youtube"
-            href={siteMetadata.youtube}
-            hoverColor="dark:hover:text-youtubeRed"
-          />
-          <SocialIcon
-            kind="tiktok"
-            href={siteMetadata.tiktok}
-            hoverColor="dark:hover:text-tiktokColor"
-          />
-          <SocialIcon
-            kind="instagram"
-            href={siteMetadata.instagram}
-            hoverColor="dark:hover:text-instagramColor"
-          />
-          <SocialIcon
-            kind="applePodcasts"
-            href={siteMetadata.applePodcastLink}
-            hoverColor="dark:hover:text-applePodcastsPurple"
-          />
-          <SocialIcon
-            kind="spotify"
-            href={siteMetadata.spotifyLink}
-            hoverColor="dark:hover:text-spotifyGreen"
-          />
-        </div>
-        <div className="mb-2  flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://www.cwarcup.com/">{siteMetadata.developer}</Link>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
-        </div>
+    <footer className="footer items-center bg-transparent p-4 text-neutral-content">
+      <div className="grid-flow-col items-center">
+        <SocialIcon
+          kind="youtube"
+          href={siteMetadata.youtube}
+          hoverColor="dark:hover:text-youtubeRed"
+        />
+        <Link href="/">{siteMetadata.title}</Link>
+        <p>{` • `}</p>
+        <p>{`© ${new Date().getFullYear()}`}</p>
+      </div>
+      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <SocialIcon
+          kind="tiktok"
+          href={siteMetadata.tiktok}
+          hoverColor="dark:hover:text-tiktokColor"
+        />
+        <SocialIcon
+          kind="instagram"
+          href={siteMetadata.instagram}
+          hoverColor="dark:hover:text-instagramColor"
+        />
+        <SocialIcon
+          kind="applePodcasts"
+          href={siteMetadata.applePodcastLink}
+          hoverColor="dark:hover:text-applePodcastsPurple"
+        />
+        <SocialIcon
+          kind="spotify"
+          href={siteMetadata.spotifyLink}
+          hoverColor="dark:hover:text-spotifyGreen"
+        />
       </div>
     </footer>
   )
