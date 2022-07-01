59f3c4b9fdb8460e838f06f0a628ea4ab221aa4b
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-01 09:25:26 -0700

    update contact with h1

--- pages/contact.js
+++ pages/contact.js
@@ -9,13 +9,16 @@ export default function Contact() {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
 
       <div className="flex h-screen flex-col items-center">
-        <PageTitle>Contact</PageTitle>
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Contact
+        </h1>
 
         <p className="prose m-4 max-w-none pb-4 text-center text-lg leading-7 text-gray-700">
           If there’s a topic you want to see covered, or a question answered directly or in a video,
           we want to hear from you! Send us an email, or DM us on Instagram, and we’ll get back to
           you!
         </p>
+
         <div>
           <div className="grid auto-rows-fr grid-cols-1 items-center gap-5 md:grid-cols-2">
             <a
--- pages/hosts.js
+++ pages/hosts.js
@@ -1,6 +1,7 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
 import { PageSEO } from '@/components/SEO'
+import PageTitle from '@/components/PageTitle'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
@@ -19,11 +20,10 @@ export default function About({ reuben, kabir }) {
         description={'about the On Your Mental Podcast hosts'}
       />
       <div className="divide-y divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:leading-10 marker:sm:text-4xl md:text-6xl md:leading-14">
-            About The Hosts
-          </h1>
-        </div>
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          About The Hosts
+        </h1>
+
         <MDXLayoutRenderer
           layout={reuben.frontMatter.layout || DEFAULT_LAYOUT}
           mdxSource={reuben.mdxSource}
