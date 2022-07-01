7a031ea7b650604e667827080ccf61f080f04fb9
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-30 18:37:31 -0700

    shrink padding on nav

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -17,7 +17,7 @@ const LayoutWrapper = ({ children }) => {
   const stuckClasses =
     'py-2 sticky top-n-1 z-50 transition-all backdrop isSticky mx-auto border-b border-slate-900/10 dark:border-slate-300/10 mb-5 w-full'
   const unstuckClasses =
-    'py-2 md:py-8 sticky top-n-1 z-50 transition-all unstuck-backdrop mx-auto border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 mb-5 w-full'
+    'py-2 md:py-4 sticky top-n-1 z-50 transition-all unstuck-backdrop mx-auto border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 mb-5 w-full'
 
   const classes = stuck ? stuckClasses : unstuckClasses
 
