bc46290d1c006c8a81f7fa4241c323fe09d4ff16
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-01 09:16:45 -0700

    PageTitle for contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,7 @@
-import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
-import he from 'he'
-import Image from '@/components/Image'
 import { BsInstagram, BsEnvelope } from 'react-icons/bs'
+import PageTitle from '@/components/PageTitle'
 
 export default function Contact() {
   return (
@@ -11,18 +9,15 @@ export default function Contact() {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
 
       <div className="flex h-screen flex-col items-center">
-        <div className="space-y-2 py-4 md:space-y-5">
-          <h1 className="text-5xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-6xl sm:leading-10 md:text-6xl md:leading-14">
-            Contact
-          </h1>
-        </div>
-        <h3 className="max-w-sm py-4 text-center text-gray-700 md:max-w-lg">
+        <PageTitle>Contact</PageTitle>
+
+        <p className="prose m-4 max-w-none pb-4 text-center text-lg leading-7 text-gray-700">
           If there’s a topic you want to see covered, or a question answered directly or in a video,
           we want to hear from you! Send us an email, or DM us on Instagram, and we’ll get back to
           you!
-        </h3>
+        </p>
         <div>
-          <div className="grid auto-rows-fr grid-cols-1 items-center gap-5 py-4 md:grid-cols-2">
+          <div className="grid auto-rows-fr grid-cols-1 items-center gap-5 md:grid-cols-2">
             <a
               className="flex transform flex-col items-center transition duration-500 hover:scale-105"
               href={`mailto:${siteMetadata.email}`}
