e709e536e88fc53ae1da66c9a32de6fb65ea6f90
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-01 19:32:39 -0700

    watch header divide-y

--- pages/watch.js
+++ pages/watch.js
@@ -31,26 +31,28 @@ export default function Episodes() {
         title="Recent episodes - On Your Mental"
         description="List of recent episodes from On Your Mental Podcast"
       />
-      <div className="flex flex-col items-center sm:content-center">
+      <div className="flex flex-col sm:content-center ">
         <div className="space-y-2 py-4 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Recent Episodes
-          </h1>
-          <div className="mt-4 grid justify-center gap-2 md:grid-cols-3">
-            {details.map((d) => (
-              <div
-                key={d.snippet.title}
-                className="rounded-lg bg-gradient-to-b from-gray-900 to-ORMblue  p-1 shadow-lg"
-              >
-                <Card
+          <div className="divide-y divide-gray-700">
+            <h1 className="pb-2 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              Recent Episodes
+            </h1>
+            <div className="grid justify-center gap-2 pt-2 md:grid-cols-3">
+              {details.map((d) => (
+                <div
                   key={d.snippet.title}
-                  title={d.snippet.title}
-                  publishedAt={d.snippet.publishedAt}
-                  imgSrc={d.snippet.thumbnails.high.url}
-                  href={`https://www.youtube.com/watch?v=${d.id.videoId}`}
-                />
-              </div>
-            ))}
+                  className="mt-5 rounded-lg bg-gradient-to-b from-gray-900  to-ORMblue p-1 shadow-lg"
+                >
+                  <Card
+                    key={d.snippet.title}
+                    title={d.snippet.title}
+                    publishedAt={d.snippet.publishedAt}
+                    imgSrc={d.snippet.thumbnails.high.url}
+                    href={`https://www.youtube.com/watch?v=${d.id.videoId}`}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
