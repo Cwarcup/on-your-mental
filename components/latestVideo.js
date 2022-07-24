4ef2de5ead5e4d54788f9f147d80163e96d7d6c3
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-24 16:05:17 -0700

    number of request is better. Desc showing up

--- components/latestVideo.js
+++ components/latestVideo.js
@@ -9,59 +9,58 @@ import htmlDecode from '@/lib/htmlDecode'
 const LatestVideo = () => {
   const [episodeTitle, setEpisodeTitle] = useState()
   const [episodeDescription, setEpisodeDescription] = useState()
+  const [videoId, setVideoId] = useState()
   const [data, setData] = useState()
   const [error, setError] = useState()
 
   useEffect(() => {
-    // get latest video from youtube, returns one video.
-    const config = {
-      method: 'get',
-      url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&channelId=UCYZNw4kprBNpxBrTcnNl_Kw&order=date&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
-      headers: {},
-    }
-
-    //fetches list of 1 latest video
-    const fetchData = async () => {
-      try {
-        const result = await axios(config)
-        setData(result.data.items)
-      } catch (error) {
-        console.log('error', error)
-        setError(error)
-      }
-    }
     fetchData()
+    console.log('use effect ran')
   }, [])
+
+  // get latest video from youtube, returns one video.
+  const config = {
+    method: 'get',
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&channelId=UCYZNw4kprBNpxBrTcnNl_Kw&order=date&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
+    headers: {},
+  }
+
+  //fetches list of 1 latest video
+  const fetchData = async () => {
+    try {
+      const fetchResult = await axios(config)
+      console.log('before setData')
+      setData(fetchResult.data.items)
+      setVideoId(fetchResult.data.items[0].id.videoId)
+      setEpisodeTitle(fetchResult.data.items[0].snippet.title)
+
+      const description = getVideoDetails(fetchResult.data.items[0].id.videoId)
+    } catch (error) {
+      console.log('error', error)
+      setError(error)
+    }
+  }
+
+  // takes in a video ID once it's been fetched from setData
+  const getVideoDetails = async (videoId) => {
+    await YTVideoIdDetails(videoId).then((result) => {
+      console.log('YTVideoIdDetails result: ', result)
+      setEpisodeDescription(getDescription(result.items[0].snippet.description))
+    })
+  }
 
   // if data is empty, show loader
   if (!data) {
     return <Loader />
   }
 
-  // get video ID from data
-  let videoId = data[0].id.videoId
-
-  // use the videoId to fetch the details of the video
-  const details = YTVideoIdDetails(videoId).then((result) => {
-    // get the title and description of the video
-    setEpisodeTitle(result.items[0].snippet.title)
-    setEpisodeDescription(getDescription(result.items[0].snippet.description))
-  })
-  console.log(episodeDescription)
-
   return (
     <div className="px-5">
       <YoutubeEmbed embedId={videoId} />
       <div className="pt-6 text-xl font-bold text-gray-900">{htmlDecode(episodeTitle)}</div>
-      <p className="prose m-4 max-w-none text-lg leading-7 text-gray-700">
-        We’ve got a banger of a Check In episode for y’all! We hope this is the perfect start to
-        your week, come n hang out, get some laughs in, some good vibes, and get at it. These
-        episodes are always fun to shoot, and this one’s not different. If you’re new to us, we’re
-        scoring our work lives, personal lives, and mental health from 1 to 10, and having an open
-        discussion about it all!
-      </p>
+      <p className="prose m-4 max-w-none text-lg leading-7 text-gray-700">{episodeDescription}</p>
     </div>
   )
 }
-
+//current
 export default LatestVideo
--- pages/index.js
+++ pages/index.js
@@ -29,14 +29,6 @@ export default function Home({ posts }) {
       }
     }
     fetchVideosList()
-
-    // get description from latest video
-    // should only contain relevant information about the episode
-    function getDescription(str) {
-      const descriptionArr = str.split('\n').filter((item) => item)
-      let index = descriptionArr.findIndex((v) => v.includes('Welcome back to On Your Mental')) + 1
-      return descriptionArr[index]
-    }
   }, [])
 
   if (!details) {
