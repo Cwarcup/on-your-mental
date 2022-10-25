86dc5ec29204d3e0b3b692fa176b3929f16d9f60
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-25 13:43:15 -0700

    youtube skeleton loader

--- components/latestVideo.js
+++ components/latestVideo.js
@@ -42,13 +42,31 @@ const LatestVideo = () => {
 
   // if data is empty, show loader
   if (!videoId || !episodeTitle || !episodeDescription) {
-    return <Loader />
+    return (
+      <div
+        role="status"
+        className="flex h-56 max-w-sm animate-pulse items-center justify-center rounded-lg bg-gray-300 dark:bg-gray-700"
+      >
+        <svg
+          className="h-12 w-12 text-gray-200 dark:text-gray-600"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 384 512"
+        >
+          <path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    )
   }
 
   return (
     <div className="px-5">
       <YoutubeEmbed embedId={videoId} />
-      <div className="pt-6 text-xl font-bold text-gray-900">{htmlDecode(episodeTitle)}</div>
+      <div className="pt-6 text-center text-xl font-bold text-gray-900 md:text-2xl">
+        {htmlDecode(episodeTitle)}
+      </div>
       {episodeDescription.map((item, index) => {
         return (
           <p className="prose m-4 max-w-none text-lg leading-7 text-gray-700" key={index}>
--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,68 @@ export default function Home({ posts }) {
   }, [])
 
   if (!details) {
-    return <Loader />
+    return (
+      <div className="flex w-full flex-col items-center">
+        <div className="space-y-2 py-4 md:space-y-5">
+          <PageTitle>Welcome to On Your Mental</PageTitle>
+
+          <p className="prose m-4 max-w-none pb-4 text-center text-lg leading-7 text-gray-700">
+            The podcast that shares candid, open, and often vulnerable conversations between & about
+            men, and what's on their minds! Mental health, relationships, feelings, outlooks on
+            life, all of it's on the table. On Your Mental is ending the stigma around men being
+            vulnerable, one conversation at a time.
+          </p>
+        </div>
+        <div
+          role="status"
+          className="max-w-xlg mb-4 flex h-56 w-full animate-pulse items-center justify-center rounded-lg bg-gray-300 dark:bg-gray-400 "
+        >
+          <svg
+            className="h-12 w-12 text-gray-200 dark:text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 384 512"
+          >
+            <path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div role="status" className="max-w-xlg mb-4 h-56 w-full animate-pulse space-y-2.5">
+          <div className="flex w-full items-center space-x-2">
+            <div className="h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-400"></div>
+            <div className="h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+            <div className="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+          </div>
+          <div className="flex w-full max-w-[480px] items-center space-x-2">
+            <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-400"></div>
+            <div className="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+            <div className="h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+          </div>
+          <div className="flex w-full max-w-[400px] items-center space-x-2">
+            <div className="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+            <div className="h-2.5 w-80 rounded-full bg-gray-200 dark:bg-gray-400"></div>
+            <div className="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+          </div>
+          <div className="flex w-full max-w-[480px] items-center space-x-2">
+            <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-400"></div>
+            <div className="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+            <div className="h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+          </div>
+          <div className="flex w-full max-w-[440px] items-center space-x-2">
+            <div className="h-2.5 w-32 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+            <div className="h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+            <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-400"></div>
+          </div>
+          <div className="flex w-full max-w-[360px] items-center space-x-2">
+            <div className="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+            <div className="h-2.5 w-80 rounded-full bg-gray-200 dark:bg-gray-400"></div>
+            <div className="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    )
   }
   return (
     <>
@@ -41,7 +102,7 @@ export default function Home({ posts }) {
         <div className="space-y-2 py-4 md:space-y-5">
           <PageTitle>Welcome to On Your Mental</PageTitle>
 
-          <p className="prose m-4 max-w-none pb-4 text-center text-lg leading-7 text-gray-700">
+          <p className="prose m-4 max-w-none pb-4 text-center text-lg leading-7 text-gray-400">
             The podcast that shares candid, open, and often vulnerable conversations between & about
             men, and what's on their minds! Mental health, relationships, feelings, outlooks on
             life, all of it's on the table. On Your Mental is ending the stigma around men being
